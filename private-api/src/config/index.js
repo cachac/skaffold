fc80306273fd0e3e60c777022493be1d00e53744
Your Name <you@example.com>
2023-02-08 01:35:41 +0000

    skaffold

--- private-api/src/config/index.js
+++ private-api/src/config/index.js
@@ -14,6 +14,8 @@ const {
 
 console.log('Env vars (con proposito de pruebas) ', JSON.parse(APP_ENV))
 
+console.log('  NODE_PORT,  APP_NAME,  NODE_ENV,  TOKEN_LIMIT,  TOKEN_SECRET :>> ',  NODE_PORT,  APP_NAME,  NODE_ENV,  TOKEN_LIMIT,  TOKEN_SECRET);
+
 export default {
   NODE_PORT,
   APP_NAME,
